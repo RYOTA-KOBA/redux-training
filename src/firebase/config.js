149b5615546bf3b570b749872be93f5d1086789e
RYOTA <kobaryo04ysh@icloud.com>
2021-04-14 15:17:12 +0900

    firebaseの設定を追加

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDXkTd7aaFTp2ZVPdtgnzc8hYva_N50xcQ',
+  authDomain: 'redux-tr.firebaseapp.com',
+  projectId: 'redux-tr',
+  storageBucket: 'redux-tr.appspot.com',
+  messagingSenderId: '176770448583',
+  appId: '1:176770448583:web:b5e86c24c00e998539d2e2',
+};
