176ee684782df7ffbb719036e1afc98375404894
RYOTA <kobaryo04ysh@icloud.com>
2021-04-16 12:32:30 +0900

    認証用のAuthコンポーネントを追加

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import { Login, Home } from './templates';
+import { SignIn, Home, SignUp, Reset } from './templates';
+import Auth from './Auth';
 
 const Router: React.FC = () => {
   return (
     <Switch>
-      <Route exact path={'/login'} component={Login} />
-      <Route exact path={'(/)?'} component={Home} />
+      <Route exact path={'/signup'} component={SignUp} />
+      <Route exact path={'/signin'} component={SignIn} />
+      <Route exact path={'/signin/reset'} component={Reset} />
+
+      <Auth>
+        <Route exact path={'(/)?'} component={Home} />
+      </Auth>
     </Switch>
   );
 };
