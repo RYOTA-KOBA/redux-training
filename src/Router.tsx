3eb2a00ececc979c9fb304473d3fbaef6e0331fd
RYOTA <kobaryo04ysh@icloud.com>
2021-04-16 17:03:39 +0900

    feat: productsの追加用のコンポーネントを追加

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import { SignIn, Home, SignUp, Reset } from './templates';
+import { SignIn, Home, SignUp, Reset, ProductEdit } from './templates';
 import Auth from './Auth';
 
 const Router: React.FC = () => {
@@ -12,6 +12,7 @@ const Router: React.FC = () => {
 
       <Auth>
         <Route exact path={'(/)?'} component={Home} />
+        <Route exact path={'/product/edit'} component={ProductEdit} />
       </Auth>
     </Switch>
   );
