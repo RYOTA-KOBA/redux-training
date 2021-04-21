727e0f0753e021dd734ed6469a784fcfed065913
RYOTA <kobaryo04ysh@icloud.com>
2021-04-21 12:13:20 +0900

    feat: signoutボタンを仮実装

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { getUserId, getUsername } from '../reducks/users/selectors';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { signOut } from '../reducks/users/operations';
 
 const Home: React.FC = () => {
+  const dispatch = useDispatch();
   const selector = useSelector((state) => state);
   const uid = getUserId(selector);
   const username = getUsername(selector);
@@ -12,6 +14,7 @@ const Home: React.FC = () => {
       <h2>Home</h2>
       <p>ID: {uid}</p>
       <p>NAME: {username}</p>
+      <button onClick={() => dispatch(signOut())}>sign out </button>
     </div>
   );
 };
